b5ed9c0b5ed6bc04348845897ca79f85375a4212
AuRocKbra <4haurock@gmail.com>
2025-06-24 20:21:07 -0300

    Projeção de multiplos conteúdos
    Aplica aprojeção de multiplos conteúdos usando o ng-content
    
    	new file:   src/app/area-financeira/compartilhados/card/card.component.css
    	new file:   src/app/area-financeira/compartilhados/card/card.component.html
    	new file:   src/app/area-financeira/compartilhados/card/card.component.ts
    	modified:   src/app/area-financeira/saldo/saldo.component.css
    	modified:   src/app/area-financeira/saldo/saldo.component.html
    	modified:   src/app/area-financeira/saldo/saldo.component.ts
    	modified:   src/app/area-financeira/transacoes/transacoes.component.css
    	modified:   src/app/area-financeira/transacoes/transacoes.component.html
    	modified:   src/app/area-financeira/transacoes/transacoes.component.ts

--- src/app/area-financeira/saldo/saldo.component.ts
+++ src/app/area-financeira/saldo/saldo.component.ts
@@ -1,13 +1,15 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { DestaqueValorNumericoDirective } from '../../compartilhados/destaque-valor-numerico.directive';
+import { CardComponent } from "../compartilhados/card/card.component";
 
 @Component({
   selector: 'app-saldo',
-  imports: [CurrencyPipe,DestaqueValorNumericoDirective],
+  imports: [CurrencyPipe, DestaqueValorNumericoDirective, CardComponent],
   templateUrl: './saldo.component.html',
   styleUrl: './saldo.component.css'
 })
 export class SaldoComponent {
+  tituloSaldo:string = "Saldo disponível:";
   saldo = input.required<number>();
 }
--- src/app/area-financeira/transacoes/transacoes.component.ts
+++ src/app/area-financeira/transacoes/transacoes.component.ts
@@ -2,13 +2,15 @@ import { Component, input } from '@angular/core';
 import { TransacaoComponent } from "./transacao/transacao.component";
 import { Transacao } from '../compartilhados/transacao.model';
 import { BotaoComponent } from "../../compartilhados/botao/botao.component";
+import { CardComponent } from '../compartilhados/card/card.component';
 
 @Component({
   selector: 'app-transacoes',
-  imports: [TransacaoComponent, BotaoComponent],
+  imports: [TransacaoComponent, BotaoComponent,CardComponent],
   templateUrl: './transacoes.component.html',
   styleUrl: './transacoes.component.css'
 })
 export class TransacoesComponent {
+  tituloTransacoes:string = "Movimentação financeira";
   transacoes = input.required<Transacao[]>();
 }
