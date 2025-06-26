6720d09fd117db9400c808740d788a26614dfae6
augusto.espindola <augusto.espindola@sesicni.com.br>
2025-06-26 13:47:55 -0300

    Finalizando uso card e iniciando uso viewChild
    Finalizando o uso do componente card em contas e iniciando o uso da viewChild em transações
    
    	modified:   src/app/area-financeira/compartilhados/card/card.component.html
    	modified:   src/app/area-financeira/contas/conta/conta.component.html
    	modified:   src/app/area-financeira/contas/contas.component.css
    	modified:   src/app/area-financeira/contas/contas.component.html
    	modified:   src/app/area-financeira/contas/contas.component.ts
    	modified:   src/app/area-financeira/saldo/saldo.component.html
    	new file:   src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.css
    	new file:   src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.html
    	new file:   src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
    	modified:   src/app/area-financeira/transacoes/transacoes.component.css
    	modified:   src/app/area-financeira/transacoes/transacoes.component.html
    	modified:   src/app/area-financeira/transacoes/transacoes.component.ts

---
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -0,0 +1,16 @@
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
+
+@Component({
+  selector: 'app-botao-adicionar-transacao',
+  imports: [BotaoComponent],
+  templateUrl: './botao-adicionar-transacao.component.html',
+  styleUrl: './botao-adicionar-transacao.component.css'
+})
+export class BotaoAdicionarTransacaoComponent {
+ modal = viewChild.required<ElementRef<HTMLDialogElement>>('modal');
+
+ abrirModal(){
+    this.modal().nativeElement.showModal();
+ }
+}
