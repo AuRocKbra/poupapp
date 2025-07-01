aa281794e44b37a74c5aabd268021c7d33615b7e
augusto.espindola <augusto.espindola@sesicni.com.br>
2025-07-01 13:30:28 -0300

    Inicinado implementação do formulário de cadastro de transação
    	modified:   src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.html
    	modified:   src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -2,17 +2,19 @@ import { Component, signal } from '@angular/core';
 import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
 import {FormsModule} from "@angular/forms";
+import { TipoTransacao } from '../../compartilhados/transacao.model';
+import { KeyValuePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent, ModalComponent,FormsModule],
+  imports: [BotaoComponent, ModalComponent,FormsModule,KeyValuePipe],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css'
 })
 export class BotaoAdicionarTransacaoComponent {
   modalAberta = signal(false);
-  
+  tiposTransacoes = TipoTransacao;
   novaTransacaoForm = {
     nome:'',
     tipo:'',
@@ -26,6 +28,6 @@ export class BotaoAdicionarTransacaoComponent {
   }
 
   aoSubmeter(){
-    console.log();
+    console.log(this.novaTransacaoForm);
   }
 }
